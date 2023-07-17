3111ed5302055f79ec7549f5b4e41be61b3af103
3str-webdev <3str.webdev@gmail.com>
2023-07-17 18:23:22 +0300

    fix: remove useless log

--- shared/hooks/use-sort-todos.ts
+++ shared/hooks/use-sort-todos.ts
@@ -3,8 +3,6 @@ import { useAppSelector } from "./redux-hooks";
 export const useSortTodos = () => {
   const todos = useAppSelector((store) => store.todosReducer.todos);
 
-  console.log(todos);
-
   if (todos.length < 2) return todos;
 
   const sortedTodos = [...todos].sort((a, b) => b.priority - a.priority);
