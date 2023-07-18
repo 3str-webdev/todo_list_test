bd8adf62125efa628bcf3698c36114d098017137
3str-webdev <3str.webdev@gmail.com>
2023-07-18 22:05:13 +0300

    fix: remove useless import

--- ui/ui-modal.tsx
+++ ui/ui-modal.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, HTMLAttributes, MouseEvent } from "react";
+import { HTMLAttributes, MouseEvent } from "react";
 import { createPortal } from "react-dom";
 import styled from "styled-components";
 
